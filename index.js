27dfec7e5fc10ed9f395579abf12fca5f602a398
gleydsonbrito <gleydsonbrito@hotmail.com>
2024-06-20 22:38:43 -0300

    adicionando cors a aplicação

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import cors from 'cors'
 import useRoutes from './routes/provaRouter.js'
 
 const app = express()
 app.set('port', process.env.PORT || 3000)
+app.use(cors)
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(useRoutes)
