467e5948e770d5920ebd23235e2579c2e787e3cc
gleydsonbrito <gleydsonbrito@hotmail.com>
2024-06-29 12:49:34 -0300

    adicionando middleware

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import useRoutes from './routes/provaRouter.js'
+import verifyCorsPolicy from './middleware/provaMiddleware.js'
 
 const app = express()
 app.set('port', process.env.PORT || 3000)
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'DELETE', 'PATCH' ],
-  allowedHeaders: ['Content-Type'] // Permite apenas Content-Type no header
+  allowedHeaders: ['Content-Type']
 }))
+app.use(verifyCorsPolicy)
 app.use(bodyParser.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(useRoutes)
