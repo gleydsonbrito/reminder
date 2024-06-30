4c3352abc72d6c0107c950d7501caccdb1279db8
gleydsonbrito <gleydsonbrito@hotmail.com>
2024-06-30 13:49:22 -0300

    corrige cors removendo objeto da funçao

--- index.js
+++ index.js
@@ -6,11 +6,7 @@ import verifyCorsPolicy from './middleware/provaMiddleware.js'
 
 const app = express()
 app.set('port', process.env.PORT || 3000)
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'DELETE', 'PATCH' ],
-  allowedHeaders: ['Content-Type']
-}))
+app.use(cors())
 app.use(verifyCorsPolicy)
 app.use(bodyParser.json())
 app.use(express.urlencoded({ extended: true }))
